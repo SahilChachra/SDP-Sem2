6eed140ab10983ffa91f078748b4b456b69c21d6
Sahil Chachra <37156032+SahilChachra@users.noreply.github.com>
2018-10-29 23:04:58 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,101 @@
+function menuprogram1()
+{
+var a=document.getElementById('menu1');
+a.style.display=("block");
+}
+
+function closemenuprogram1()
+{
+var a=document.getElementById('menu1');
+a.style.display=("none");
+}
+
+function btech()
+{
+var a=document.getElementById('btech');
+a.style.display=("block");
+
+var c=document.getElementById('mtech1');
+c.style.display=("none");
+
+var b=document.getElementById('mtech');
+b.style.display=("none");
+
+var d=document.getElementById('mtech2');
+d.style.display=("none");
+var e=document.getElementById('phd');
+e.style.display=("none");
+}
+
+function mtech()
+{
+var a=document.getElementById('btech');
+a.style.display=("none");
+
+var c=document.getElementById('mtech1');
+c.style.display=("none");
+
+var b=document.getElementById('mtech');
+b.style.display=("block");
+
+var d=document.getElementById('mtech2');
+d.style.display=("none");
+
+var e=document.getElementById('phd');
+e.style.display=("none");
+}
+
+function mtech1()
+{
+var a=document.getElementById('mtech');
+a.style.display=("none");
+
+var b=document.getElementById('btech');
+b.style.display=("none");
+
+var c=document.getElementById('mtech1');
+c.style.display=("block");
+
+var d=document.getElementById('mtech2');
+d.style.display=("none");
+
+var e=document.getElementById('phd');
+e.style.display=("none");
+}
+
+function mtech2()
+{
+var a=document.getElementById('mtech');
+a.style.display=("none");
+
+var b=document.getElementById('btech');
+b.style.display=("none");
+
+var c=document.getElementById('mtech1');
+c.style.display=("none");
+
+var d=document.getElementById('mtech2');
+d.style.display=("block");
+
+var e=document.getElementById('phd');
+e.style.display=("none");
+}
+
+function phd()
+{
+var a=document.getElementById('mtech');
+a.style.display=("none");
+
+var b=document.getElementById('btech');
+b.style.display=("none");
+
+var c=document.getElementById('mtech1');
+c.style.display=("none");
+
+var d=document.getElementById('mtech2');
+d.style.display=("none");
+
+var e=document.getElementById('phd');
+e.style.display=("block");
+}
+
